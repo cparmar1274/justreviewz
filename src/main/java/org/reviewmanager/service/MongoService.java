c5f2bad708d6f6b4a3f9ee0827ee3f096ae73ca0
chirag parmar <cccchirag@gmail.com>
2019-07-30 14:14:02 -0400

    changes

--- src/main/java/org/reviewmanager/service/MongoService.java
+++ src/main/java/org/reviewmanager/service/MongoService.java
@@ -40,7 +40,7 @@ public class MongoService {
 													// which the user is defined
 			//mongodb://admin:password1@ds255857.mlab.com:55857/heroku_4rqpl385/?authSource=db1
 			//mongodb://localhost:27017/?authSource=db1
-			MongoClientURI uri = new MongoClientURI("mongodb://admin:password1@ds255857.mlab.com:55857/heroku_4rqpl385/?authSource=db1");
+			MongoClientURI uri = new MongoClientURI("mongodb://admin:password1@ds255857.mlab.com:55857/heroku_4rqpl385?authSource=db1");
 			mongoClient = new MongoClient(uri);
 			mongoDB = mongoClient.getDatabase(database);
 			// create Index
